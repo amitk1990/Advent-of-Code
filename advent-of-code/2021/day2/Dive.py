f09042da863115abee0fcf21f643781c183e433f
Punteramit <amitk26467@gmail.com>
2021-12-02 01:09:13 -0500

    day 2 Dive

---
+++ advent-of-code/2021/day2/Dive.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append("advent-of-code/2021/utils")
+
+from file_utils import read_lines
+
+dict = {"forward": +1, "down": +1, "up": -1 }
+
+def dive(nums):
+  horizontal_position , depth = 0, 0
+  for direction, value in nums:
+    if direction == "forward":
+      horizontal_position += dict[direction]*value
+    else:
+      depth += dict[direction]*value
+      
+  return horizontal_position*depth
+
+def dive_with_aim(nums):
+  horizontal_position , depth = 0, 0
+  aim = 0
+  for direction, value in nums:
+    if direction == "forward":
+      horizontal_position += dict[direction]*value
+      depth += aim*value
+    elif direction == "down":
+      aim += value
+    else:
+      aim += dict[direction]*value
+      
+  return horizontal_position*depth
+
+if __name__ == '__main__':
+  input = read_lines("advent-of-code/2021/day2/input.txt")
+
+  result = []
+  for item in input:
+    temp = item.rstrip().split(" ")
+    result.append((temp[0], int(temp[1])))
+  
+  res = dive(result)
+  print(f"part 1 {res}") # 1989014
+  res = dive_with_aim(result)
+  print(f"part 2 {res}") # 2006917119
+  
+    
--- advent-of-code/2021/utils/file_utils.py
+++ advent-of-code/2021/utils/file_utils.py
@@ -1,6 +1,32 @@
 def read(filename):
   input = []
-  with open(filename) as file:
+  with open(filename, 'r') as file:
     input = file.read().split("\n")
     
   return input
+
+def read_single_line(filename):
+  input = []
+  with open(filename, 'r') as file:
+    input = file.readline().split(",")
+  return input
+
+def read_lines(filename):
+  input = []
+  with open(filename, 'r') as file:
+    input = file.readlines()
+
+  return input
+
+def read_with_directions(filename):
+  input = []
+  with open(filename, 'r') as file:
+    input = file.readlines()
+  
+  result = []
+  for line in input:
+    direction = line.rstrip().split(",")
+    for item in direction:
+      result.append((item[:1], int(item[1:])))
+
+  return result
