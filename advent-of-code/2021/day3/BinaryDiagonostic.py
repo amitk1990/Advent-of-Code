d49355583cf81ce55a2d702a295be27b5f41e8ad
Punteramit <amitk26467@gmail.com>
2021-12-03 16:34:39 -0500

    clean up and reduce complexity

--- advent-of-code/2021/day3/BinaryDiagonostic.py
+++ advent-of-code/2021/day3/BinaryDiagonostic.py
@@ -3,40 +3,26 @@ sys.path.append("advent-of-code/2021/utils")
 
 from file_utils import read_lines
 
-def find_most_frequent(dict):
-  count = 0
-  most_frequent = 0
-  for key in dict:
-    if dict[key] >= count:
-      count = dict[key]
-      most_frequent = key
-  
-  return most_frequent
-
 def calculate_power_consumption(diagonostic_report):
-  row_length = len(diagonostic_report)
-  column_length = len(diagonostic_report[0])
-  gamma_rate = ""
-  epsilon_rate = ""
-  for column in range(0, column_length):
-    dict = {}
-    i = 0
-    while i < row_length:
-      item = diagonostic_report[i][column]
-      dict[item] = dict.get(item, 0) + 1
-      i += 1
-    most_frequent = find_most_frequent(dict)
+  row_length, column_length = len(diagonostic_report), len(diagonostic_report[0])
+  gamma_rate = epsilon_rate =  ""
 
-    least_frequent = "0"
-    if most_frequent == "1":
-      least_frequent = "0"
+  for column in range(0, column_length):
+    index, input_bits = 0, []
+    while index < row_length:
+      item = diagonostic_report[index][column]
+      input_bits.append(item)
+      index += 1
+
+    if (input_bits.count("1") >= input_bits.count("0")):
+      most_frequent, least_frequent = "1", "0"
     else:
-      least_frequent = "1"
+      least_frequent, most_frequent = "1", "0"
 
     gamma_rate += str(most_frequent)
     epsilon_rate += str(least_frequent)
   
-  # convert to decimal and return product
+  # convert from binary to decimal and return product
   return (int(gamma_rate, 2) * int(epsilon_rate, 2))
 
 if __name__ == '__main__':
